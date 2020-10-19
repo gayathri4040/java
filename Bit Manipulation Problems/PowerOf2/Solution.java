bea3d3cf017e90593b137ded0f5a9c8ee5bfd5a6
Gayathri <31845757+gayathri4040@users.noreply.github.com>
2020-10-20 04:20:37 +1100

    Using long datatype

--- Bit Manipulation Problems/PowerOf2/Solution.java
+++ Bit Manipulation Problems/PowerOf2/Solution.java
@@ -7,19 +7,19 @@ import java.math.BigInteger;
 
 class Solution {
 
-    static boolean powerOf2(BigInteger n) {
+    static boolean powerOf2(long n) {
         //n & n-1 unsets the right most set (least significant) bit to 0
         //the numbers which are power of 2, have only one significant bit in their binary representation
         //if that one bit is unset, the number becomes zero
         //if it doesn't become zero then the number is not a power of 2
-        if((n.and(n.subtract(BigInteger.ONE))).equals(BigInteger.ZERO))
+        if((n & n-1) == 0)
             return true;
         return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        BigInteger n = new BigInteger(sc.next());
+        long n = Long.parseLong(sc.next());
         System.out.println(powerOf2(n));
 
         sc.close();
