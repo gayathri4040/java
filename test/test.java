6c6c1588589355bcb59a3845cd6263651a24a8a4
Gayathri <31845757+gayathri4040@users.noreply.github.com>
2020-10-30 03:39:00 +1100

    Sorted

---
+++ test/test.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class test{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Hello world!!");
+    sc.close();
+  }
+}
